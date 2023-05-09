516abaac39c3d83de85040d05ad3d6e3c1d11583
Robert <robert__souza@hotmail.com>
2023-05-08 23:54:50 -0300

    first commit

---
+++ src/test/java/com/applogin/login/homecontrollerTests.java
@@ -0,0 +1,13 @@
+package com.applogin.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class homecontrollerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
